a65440a7dabe35a26d8c783a996f8b8cf8c17380
matt <matthilton2005@gmail.com>
2021-09-01 23:20:38 +0100

    added deploy.ps1

---
+++ deploy.ps1
@@ -0,0 +1,22 @@
+Function Get-Deb
+{
+param(
+[string]$package
+)
+$Buildpath=Join-Path -Path (Join-Path -Path $pwd -ChildPath $Package) -ChildPath 'build'
+If (Test-path $buildpath)
+{
+$deb=(Get-childitem -Path ('{0}\*.deb' -f $Buildpath) -file).fullname
+}
+return $deb
+}
+$packages = @('aasdk', 'qt-gstreamer', 'openauto', 'pulseaudio', 'dash')
+Foreach ($package in $packages) {
+    $deb = Get-Deb -Package $package
+    IF ($deb) {
+        $packagename = ((get-item $deb).name -split '_')[0]
+        reprepro --basedir /home/opendsh/packagerepo/debian remove buster $packagename
+        reprepro --basedir /home/opendsh/packagerepo/debian remove bullseye $packagename
+        reprepro --basedir /home/opendsh/packagerepo/debian includedeb buster $deb
+    }
+}
